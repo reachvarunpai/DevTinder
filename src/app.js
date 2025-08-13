97e9f7fa12756487ed3a1e1dbc9ea8d0a6397060
reachvarunpai <reachvarunpai@gmail.com>
2025-08-13 16:44:45 +0530

    writing-auth-middleware

--- src/app.js
+++ src/app.js
@@ -6,18 +6,6 @@ app.get("/user", (req, res) => {
     res.send({firstNmae: "Varun", lastName: "Pai"});
 });
 
-app.post("/user", (req, res) => {
-    res.send("Data Successfully saved to Database.");
-});
-
-app.delete("/user", (req, res) => {
-    res.send("Deleted Successfully...");
-});
-
-app.use("/test", (req, res) => {
-    res.send("Hello !");
-});
-
 app.listen(7777, () => {
     console.log("Server is successfully listening on Port 7777....");
 });
