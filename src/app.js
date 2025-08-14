8e9946da3f4e59b7a37d9710e9bf84798beef994
reachvarunpai <reachvarunpai@gmail.com>
2025-08-14 17:16:43 +0530

    delete and update

--- src/app.js
+++ src/app.js
@@ -42,6 +42,30 @@ try {
     }
 });
 
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userId;
+try {
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
+        }
+     catch (err) {
+        res.status(400).send("Something went wrong")
+    }
+});
+
+app.patch("/user", async (req, res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+try {
+        const user = await User.findByIdAndUpdate({ _id: userId }, data);
+        res.send("User updated successfully");
+        }
+     catch (err) {
+        res.status(400).send("Something went wrong")
+    }
+});
+
+
 connectDB()
     .then(() => {
         console.log("Database connection established..");
