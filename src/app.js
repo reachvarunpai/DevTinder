8fabec325a9fe632d662e9c52fe224c9818106f9
reachvarunpai <reachvarunpai@gmail.com>
2025-08-15 19:57:43 +0530

    userschema updating

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const { validateSignUpData } = require("./utils/validation");
 const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+const { userAuth } = require("./middlewares/auth");
 
 app.use(express.json());
 app.use(cookieParser());
@@ -41,14 +42,15 @@ app.post("/login", async (req, res) => {
         if (!user) {
             throw new Error("Invalid Credentials");
         }
-        const isPasswordValid = await bcrypt.compare(password, user.password);
+        const isPasswordValid = await user.validatePassword(password);
 
         if(isPasswordValid) {
 
-        const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$790");
-        console.log(token);
+        const token = await user.getJWT();
 
-            res.cookie("token", token);
+            res.cookie("token", token, {
+                expires: new Date(Date.now() + 8 * 3600000),
+            });
             res.send("login Successfull!!!");
         }
         else {
@@ -59,92 +61,20 @@ app.post("/login", async (req, res) => {
    }
 });
 
-app.get("/profile", async (req, res) => {
-   try { const cookies = req.cookies;
-
-    const { token } = cookies;
-    if (!token) {
-        throw new Error("Invalid Token");
-    }
-
-    const decodeMessage = await jwt.verify(token, "DEV@Tinder$790");
-
-    const { _id } = decodeMessage;
-    console.log("Loggeed In user is: " + _id);
-
-    const user = await User.findById(_id);
-    if(!user) {
-        throw new Error("User does not Exist");
-    }
+app.get("/profile", userAuth, async (req, res) => {
+   try { 
+    const user = req.user;
     res.send(user);
    } catch (err) {
     res.status(400).send("ERROR :" + err.message);    
    }
 });
 
-app.get("/user", async (req, res) => {
-    const userEmail = req.body.emailId;
-    try {
-        const user = await User.findOne({ emailId: userEmail });
-        res.send(user);
-        // const users = await User.find({ emailId: userEmail });
-        // if(users.length === 0) {
-        //     res.status(404).send("User not found");
-        // } else {
-        // res.send(users);
-        // }
-    } catch (err) {
-        res.status(400).send("Something went wrong")
-    }
-});
+app.post("/sendConnectionRequest", userAuth, async (req, res) => {
+    const user = req.user;
+     console.log("Sending a connection request");
 
-app.get("/feed", async (req, res) => {
-try {
-        const users = await User.find({});
-        res.send(users);
-        }
-     catch (err) {
-        res.status(400).send("Something went wrong")
-    }
-});
-
-app.delete("/user", async (req, res) => {
-    const userId = req.body.userId;
-try {
-        const user = await User.findByIdAndDelete(userId);
-        res.send("User deleted successfully");
-        }
-     catch (err) {
-        res.status(400).send("Something went wrong")
-    }
-});
-
-app.patch("/user/:userId", async (req, res) => {
-    const userId = req.params?.userId;
-    const data = req.body;
-
-    try {
-        const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
-        const isUpdateAllowed = Object.keys(data).every((k) =>
-            ALLOWED_UPDATES.includes(k)
-        );
-        if (!isUpdateAllowed) {
-            return res.status(400).send("Update not allowed");
-        }
-        if (data?.skills.length > 10) {
-            throw new Error("Skills cannot be more than 10");
-        }
-        const user = await User.findByIdAndUpdate(userId, data, {
-                returnDocument: "after",
-                runValidators: true,
-            });
-        if (!user) {
-            return res.status(404).send("User not found");
-        }
-        res.send("User updated successfully");
-    } catch (err) {
-        res.status(400).send("UPDATE FAILED: " + err.message);
-    }
+     res.send(user.firstName + "sent the connect request");
 });
 
 connectDB()
