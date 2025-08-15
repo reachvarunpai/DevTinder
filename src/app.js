e8bc9b12b3f4a3511667b1e1921d441fa33733bb
reachvarunpai <reachvarunpai@gmail.com>
2025-08-15 17:13:40 +0530

    created login API

--- src/app.js
+++ src/app.js
@@ -30,6 +30,27 @@ app.post("/signup", async (req, res) => {
    }
 });
 
+app.post("/login", async (req, res) => {
+    try {
+        const { emailId, password } = req.body;
+
+        const user = await User.findOne({ emailId: emailId });
+        if (!user) {
+            throw new Error("Invalid Credentials");
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(isPasswordValid) {
+            res.send("login Successfull!!!");
+        }
+        else {
+            throw new Error("Invalid Credentials")
+        }
+        } catch (err) {
+    res.status(400).send("ERROR :" + err.message);    
+   }
+});
+
 app.get("/user", async (req, res) => {
     const userEmail = req.body.emailId;
     try {
