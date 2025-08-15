f31880da4da9f0321f57bfe04c159612db4f7d91
reachvarunpai <reachvarunpai@gmail.com>
2025-08-15 18:11:00 +0530

    cookie and token

--- src/app.js
+++ src/app.js
@@ -4,8 +4,11 @@ const app = express();
 const User = require("./models/user");
 const { validateSignUpData } = require("./utils/validation");
 const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.post("/signup", async (req, res) => {
     try {
@@ -41,12 +44,40 @@ app.post("/login", async (req, res) => {
         const isPasswordValid = await bcrypt.compare(password, user.password);
 
         if(isPasswordValid) {
+
+        const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$790");
+        console.log(token);
+
+            res.cookie("token", token);
             res.send("login Successfull!!!");
         }
         else {
             throw new Error("Invalid Credentials")
         }
         } catch (err) {
+    res.status(400).send("ERROR :" + err.message);    
+   }
+});
+
+app.get("/profile", async (req, res) => {
+   try { const cookies = req.cookies;
+
+    const { token } = cookies;
+    if (!token) {
+        throw new Error("Invalid Token");
+    }
+
+    const decodeMessage = await jwt.verify(token, "DEV@Tinder$790");
+
+    const { _id } = decodeMessage;
+    console.log("Loggeed In user is: " + _id);
+
+    const user = await User.findById(_id);
+    if(!user) {
+        throw new Error("User does not Exist");
+    }
+    res.send(user);
+   } catch (err) {
     res.status(400).send("ERROR :" + err.message);    
    }
 });
