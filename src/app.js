159420b7b09804f5a910cea8ff69a19b08da7715
reachvarunpai <reachvarunpai@gmail.com>
2025-08-15 16:53:37 +0530

    validation and encrypt

--- src/app.js
+++ src/app.js
@@ -2,18 +2,31 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
-const user = require("./models/user");
-const { ReturnDocument } = require("mongodb");
+const { validateSignUpData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-   const user = new User(req.body);
-   try {
+    try {
+    // Validation of Data
+    validateSignUpData(req);
+
+    const { firstName, lastName, emailId, password } = req.body;
+    // Encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
+
+   const user = new User({
+    firstName,
+    lastName,
+    emailId,
+    password: passwordHash
+   });
+   
     await user.save();
     res.send("User added successfully");
    } catch (err) {
-    res.status(400).send("Error saving the user" + err.message);    
+    res.status(400).send("ERROR :" + err.message);    
    }
 });
 
