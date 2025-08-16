a222fa22202d661e68d8fa36012a1f65463d8e6f
reachvarunpai <reachvarunpai@gmail.com>
2025-08-16 18:09:53 +0530

    profile/password

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const profileRouter = express.Router();    
 const { userAuth } = require("../middlewares/auth");
-const { validateEditProfileData } = require("../utils/validation")
+const { validateEditProfileData } = require("../utils/validation");
+const bcrypt = require("bcryptjs");
 
 profileRouter.get("/profile/view", userAuth, async (req, res) => {
    try { 
@@ -27,6 +28,27 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     message: `${loggedInUser.firstName}, your profile updated successfully`,
     data: loggedInUser,
     });
+   } catch (err) {
+    res.status(400).send("ERROR :" + err.message);    
+   }
+});
+
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+   try { 
+        const { oldPassword, newPassword } = req.body;
+      const user = req.user;
+      if (!oldPassword || !newPassword) {
+         return res.status(400).send("Both oldPassword and newPassword are required");
+      }
+      const isMatch = await bcrypt.compare(oldPassword, user.password);
+      if (!isMatch) {
+         return res.status(400).send("Old password is incorrect");
+      }
+      const hashedPassword = await bcrypt.hash(newPassword, 10);
+      user.password = hashedPassword;
+      await user.save();
+
+      res.send("Password updated successfully ✅");
    } catch (err) {
     res.status(400).send("ERROR :" + err.message);    
    }
