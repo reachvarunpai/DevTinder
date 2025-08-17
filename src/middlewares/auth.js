3251d8e7837068575a0c0878e33b070219da85db
reachvarunpai <reachvarunpai@gmail.com>
2025-08-17 17:24:15 +0530

    Queries and indexs in DB & handle corner cases

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,22 +4,22 @@ const User = require("../models/user");
 const userAuth = async (req, res, next) => {
     try {
         const { token } = req.cookies;
-        if(!token) {
-            throw new Error("Token is not valid!!!!!!");
+        if (!token) {
+            return res.status(401).send("Token is not valid!");
         }
-        const decodedObj = await jwt.verify(token, "DEV@Tinder$790");
 
+        const decodedObj = await jwt.verify(token, "DEV@Tinder$790");
         const { _id } = decodedObj;
 
         const user = await User.findById(_id);
-        if(!user) {
-            throw new Error("User not found");
+        if (!user) {
+            return res.status(404).send("User not found");
         }
 
         req.user = user;
         next();
     } catch (err) {
-        res.send(400).send("ERROR: " + err.message)
+        return res.status(400).send("ERROR: " + err.message);
     }
 };
 
--- src/models/user.js
+++ src/models/user.js
@@ -8,6 +8,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         minLength: 4,
+        maxLength: 50,
     },
     lastName: {
         type: String,
